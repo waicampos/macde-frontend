72757cf953930efa29b026caab4011857291afd3
waicampos <dyego.campos@ifsc.edu.br>
2023-06-02 12:19:16 -0300

    Resolvido problema de atualização automática dos gráficos. Utilizado watch deep true.

--- src/views/UserHistoryPage.vue
+++ src/views/UserHistoryPage.vue
@@ -10,17 +10,15 @@
       <v-col cols="12">
         <MyLine
           id="my-line-historic-chart-demand-id"
-          :data="chartDataDemand"
-          :options="chartOptions"
+          :data="chartDemand"
+          :options="this.chartOptions"
         />
       </v-col>
-    </v-row>
-
-      <v-row class="flex-1-0 ma-2 pa-2">
+    
         <v-col cols="12">
           <MyLine
             id="my-line-historic-chart-energy-id"
-            :data="chartDataEnergy"
+            :data="chartEnergy"
             :options="chartOptions"
           />
       </v-col>
@@ -48,67 +46,15 @@ import { mapGetters } from 'vuex'
     components: {FileUploader, TableData, MyLine},
     data() {
       return {
-        
+        chartEnergy: {datasets: []},
+        chartDemand: {datasets: []}
       }
     },
     computed: {
       ...mapGetters({
           data_file: 'get_user_data_history',
       }),
-
-      chartDataDemand() {
-        let arr = {datasets: []}
-        arr.datasets.push({
-            label: 'Ponta',
-            data: this.data_file,
-            parsing: {
-                xAxisKey: 'date',
-                yAxisKey: 'peak_demand',
-            },
-            borderColor: '#36A2EB',
-            backgroundColor: '#9BD0F5'
-        })
-
-        arr.datasets.push({
-            label: 'Fora de Ponta',
-            data: this.data_file,
-            parsing: {
-                xAxisKey: 'date',
-                yAxisKey: 'off_peak_demand',
-            },
-            borderColor: '#B71C1C',
-            backgroundColor: '#E53935'
-        })
-        return arr
-
-      },
-      
-      chartDataEnergy() {
-        let arr = {datasets: []}
-        arr.datasets.push({
-            label: 'Ponta',
-            data: this.data_file,
-            parsing: {
-                xAxisKey: 'date',
-                yAxisKey: 'peak_energy',
-            },
-            borderColor: '#E65100',
-            backgroundColor: '#FB8C00'
-        })
-
-        arr.datasets.push({
-            label: 'Fora de Ponta',
-            data: this.data_file,
-            parsing: {
-                xAxisKey: 'date',
-                yAxisKey: 'off_peak_energy',
-            },
-            borderColor: '#1B5E20',
-            backgroundColor: '#43A047'
-        })
-        return arr
-      },
-
+    
       chartOptions() {
         return {
           responsive: true,
@@ -123,6 +69,72 @@ import { mapGetters } from 'vuex'
         // Função Date o mês inicia em 0
         return new Date(curr[2], curr[1] - 1, curr[0])
       },
-    }
+
+      chartDataDemand() {
+        this.chartDemand = {datasets: []}
+        this.chartDemand.datasets.push({
+            label: 'Ponta',
+            data: this.data_file,
+            parsing: {
+                xAxisKey: 'date',
+                yAxisKey: 'peak_demand',
+            },
+            borderColor: '#36A2EB',
+            backgroundColor: '#9BD0F5'
+        })
+
+        this.chartDemand.datasets.push({
+            label: 'Fora de Ponta',
+            data: this.data_file,
+            parsing: {
+                xAxisKey: 'date',
+                yAxisKey: 'off_peak_demand',
+            },
+            borderColor: '#B71C1C',
+            backgroundColor: '#E53935'
+        })
+      },
+      
+      chartDataEnergy() {
+        this.chartEnergy = {datasets: []}
+        this.chartEnergy.datasets.push({
+            label: 'Ponta',
+            data: this.data_file,
+            parsing: {
+                xAxisKey: 'date',
+                yAxisKey: 'peak_energy',
+            },
+            borderColor: '#E65100',
+            backgroundColor: '#FB8C00'
+        })
+
+        this.chartEnergy.datasets.push({
+            label: 'Fora de Ponta',
+            data: this.data_file,
+            parsing: {
+                xAxisKey: 'date',
+                yAxisKey: 'off_peak_energy',
+            },
+            borderColor: '#1B5E20',
+            backgroundColor: '#43A047'
+        })
+      },
+
+    },
+    mounted(){
+      this.chartDataDemand()
+      this.chartDataEnergy()
+    },
+    watch: {
+      data_file: {
+        handler() {
+          console.log("Watch")
+          this.chartDataDemand()
+          this.chartDataEnergy()
+        },
+        deep: true,
+        imediate: true,
+      }
+    },
   }
 </script>
