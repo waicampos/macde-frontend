95b46a5f15049beef1ddc1eba4a0dd3ac440d1ca
waicampos <dyego.campos@ifsc.edu.br>
2023-06-13 14:17:55 -0300

    Ajuste no texto da página. Adiciona um condição para mostrar gráficos e tabela apenas quando os dados forem carregados.

--- src/views/UserHistoryPage.vue
+++ src/views/UserHistoryPage.vue
@@ -2,22 +2,57 @@
   <div class="d-flex flex-column">
     <v-row class="flex-1-0 ma-2 pa-2">
       <v-col cols="12">
+        <v-card
+          elevation="0"
+        >
+          <v-card-title>Etapa Entrada dos dados históricos</v-card-title>               
+          <v-card-subtitle>Entrada dos dados que serão utilizados nas simulações.</v-card-subtitle>               
+          <v-divider></v-divider>
+          <v-card-text>
+              <v-row class="text-center pa-0">
+                <v-col>
+                    <p class="text-justify"> 
+                        Toda a aplicação MACDE é baseada no histórico de dados do consumidor. Esses dados estão disponíveis na fatura de energia e devem ser compilados em um arquivo
+                        que será enviado para a aplicação.
+                        <br><br>
+                    </p>
+                    <p class="text-justify"> 
+                        Os dados podem ser enviados clicando ou arrastando o arquivo diretamente na caixa de entrada abaixo. <br><br>
+                    </p>
+                    <p class="text-justify"> 
+                        É possível carregar os dados, clicando no botão CARREGAR MODELO. Ao carregar um arquivo corretamente os dados do arquivo serão apresentados de
+                        forma gráfica e tabular.
+                    </p>                    
+                </v-col>
+              </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row class="flex-1-0 ma-0 pa-0">
+      <v-col cols="12 text-center">
         <v-btn 
-        @click="load_standard_user_historic" 
-        elevation="4" 
-        size="small">
-          Carregar Modelo
+          @click="load_standard_user_historic" 
+          color="warning"
+          elevation="4" 
+          size="small"
+        >
+          Carregar Exemplo
         </v-btn>
       </v-col>
     </v-row>
-    
-     <v-row class="flex-1-0 ma-2 pa-2">
+            
+    <v-row class="flex-1-0 ma-2 pa-2">
       <v-col cols="12">
         <FileUploader  store_dispatch_name="data_history/load_user_data_history"/>
       </v-col>
     </v-row>
 
-    <v-row class="flex-1-0 ma-2 pa-2">
+    <v-row 
+      v-if="data_file.length"
+      class="flex-1-0 ma-2 pa-2"
+    >
       <v-col cols="12" lg="6">
         <v-sheet rounded="lg" min-height="300">
           <MyLine
@@ -39,7 +74,10 @@
       </v-col>
     </v-row>    
 
-    <v-row class="flex-1-0 ma-2 pa-2">
+    <v-row 
+      v-if="data_file.length"
+      class="flex-1-0 ma-2 pa-2"
+    >
       <v-col cols="12">
         <TableData />
       </v-col>
