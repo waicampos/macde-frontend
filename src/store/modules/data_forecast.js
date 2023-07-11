b7ea5f3bee1e3c94e6342659c5f47af389778e0f
waicampos <dyego.campos@ifsc.edu.br>
2023-07-11 19:13:14 -0300

    Criado método na store forecast para retornar valores previstos pelo nome informado. Criado key demand nos dados previstos.

--- src/store/modules/data_forecast.js
+++ src/store/modules/data_forecast.js
@@ -8,6 +8,20 @@ export default {
       get_forecasted_data(state) {
         return state.forecasted_data
       },
+      
+      get_forecasted_data_by_key: (state) => (key) => {
+        if(!key) {
+          return state.forecasted_data
+        }
+
+        if(state.forecasted_data.length) {
+          if(Object.keys(state.forecasted_data[0]).includes(key)) {
+            return state.forecasted_data.map(item => item[key])
+          }
+        }
+        return []
+      },
+
       get_chosen_forecast_model(state) {
         return state.chosen_forecast_model
       }
@@ -16,14 +30,19 @@ export default {
       set_forecasted_data(state, payload) {
         state.forecasted_data = payload
       },  
+
       set_chosen_forecast_model(state, payload) {
         state.chosen_forecast_model = payload
       }
     },
     actions: {
       set_forecasted_data({ commit }, payload) {
+        payload.map(item => {        
+          item.demand = Math.max(item.peak_demand, item.off_peak_demand)
+        })
         commit("set_forecasted_data", payload)
       },
+
       set_chosen_forecast_model({ commit }, payload) {
         commit('set_chosen_forecast_model', payload)
       }
