454f5c5ea98210fa32a1c90723d83f8f466c0caf
waicampos <dyego.campos@ifsc.edu.br>
2023-08-18 09:08:41 -0300

    Criado componente para mostrar mensagens.

---
+++ src/components/MessageViewer.vue
@@ -0,0 +1,52 @@
+<template>
+    <Transition                               
+        name="fade"
+    >
+        <v-alert      
+            v-if="show"       
+            density="compact"
+            :type="msg.type"
+            :title="msg.message"
+            :text="msg.code"
+        ></v-alert>
+    </Transition>  
+</template>
+
+<script>
+
+    export default {
+        name: "MessageViewer",
+        data() {
+            return {
+                show: false,
+            }
+        },
+
+        emits: {
+            msg_shown: null,
+        },
+        
+        props: {
+            msg: Object
+        },
+
+        mounted() {
+            this.show = true
+            setTimeout(() => this.show = false, 3000)
+            setTimeout(() => this.$emit('msg_shown'), 3700)
+
+        }
+    }
+</script>
+
+<style>
+    .fade-enter-from,
+    .fade-leave-to{
+        opacity: 0;
+    }
+
+    .fade-enter-active,
+    .fade-leave-active{
+        transition: opacity 2s;
+    }
+</style>
