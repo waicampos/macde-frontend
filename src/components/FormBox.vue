016c941052b1dcfaeb437a7299d6ec56e8e70df7
waicampos <dyego.campos@ifsc.edu.br>
2023-08-21 19:25:19 -0300

    Criado componente caixa padrão para formulário.

---
+++ src/components/FormBox.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <v-card class="elevation-0">
+            <v-card-item>
+                <v-card-title>
+                    <slot name="title">                        
+                        {{ title }}
+                    </slot>
+                </v-card-title>
+                <v-card-subtitle>                    
+                    <slot name="subtitle">
+                        {{ subtitle }}
+                    </slot>                    
+                </v-card-subtitle>
+            </v-card-item>
+
+            <v-card-text>                
+                <slot name="message">{{ message }}</slot>                    
+                
+                <slot name="default"></slot>                        
+                    
+            </v-card-text>
+
+            <v-card-actions>
+                <v-btn 
+                    prepend-icon="mdi-content-save"
+                    class="text-none"
+                    color="grey-lighten-1"
+                    variant="flat"
+                    @click="$emit('save')"
+                >
+                    Salvar
+                </v-btn>
+                <v-btn 
+                    class="text-none"
+                    color="grey-lighten-3"
+                    variant="flat"
+                    @click="$emit('cancel')"
+                    prepend-icon="mdi-cancel"
+                >
+                    Cancelar
+                </v-btn>                
+            </v-card-actions>
+        </v-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: {
+                type: String,
+                default: '',
+            },
+            subtitle: {
+                type: String,
+                default: '',
+            },
+            message: {
+                type: String,
+                default: '',
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
